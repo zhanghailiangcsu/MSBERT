f50df4f7631f3b696ec2cf7cef5d6830ba283d62
zhanghailiangcsu <2352434994@qq.com>
2023-10-24 17:51:31 +0800

    Update LoadGNPS.py

--- data/LoadGNPS.py
+++ data/LoadGNPS.py
@@ -20,7 +20,10 @@ from matchms.filtering import select_by_mz
 from matchms.filtering import normalize_intensities
 from matchms.filtering import require_minimum_number_of_peaks
 
-def count_annotations(spectra):
+def CountAnnotations(spectra):
+    '''
+    
+    '''
     inchi_lst = []
     smiles_lst = []
     inchikey_lst = []
@@ -40,32 +43,32 @@ def count_annotations(spectra):
     print("Inchikeys:", inchikey_count, "--", 
           len(set([x[:14] for x in inchikey_lst if x])), "unique (first 14 characters)")
 
-def apply_filters(s):
+def ApplyFilters(s):
     s = default_filters(s)
     s = derive_adduct_from_name(s)
     s = add_parent_mass(s, estimate_from_adduct=True)
     return s
 
-def clean_metadata(s):
+def CleanMetadata(s):
     s = harmonize_undefined_inchikey(s)
     s = harmonize_undefined_inchi(s)
     s = harmonize_undefined_smiles(s)
     s = repair_inchi_inchikey_smiles(s)
     return s
 
-def clean_metadata2(s):
+def CleanMetadata2(s):
     s = derive_inchi_from_smiles(s)
     s = derive_smiles_from_inchi(s)
     s = derive_inchikey_from_inchi(s)
     return s
 
-def minimal_processing(spectrum):
+def MinimalProcessing(spectrum):
     spectrum = normalize_intensities(spectrum)
     spectrum = select_by_mz(spectrum, mz_from=10.0, mz_to=1000.0)
     spectrum = require_minimum_number_of_peaks(spectrum, n_required=5)
     return spectrum
 
-def count_formulas(spectrums):
+def CountFormulas(spectrums):
     formulas = []
     name_to_formulas = []
     for spec in tqdm(spectrums):
@@ -74,7 +77,7 @@ def count_formulas(spectrums):
             name_to_formulas.append(spec.get("compound_name") + "---" + spec.get("formula"))
     return len(formulas),len(list(set(formulas)))
 
-def separate_spec(spectrums):
+def SeparateSpec(spectrums):
     spectrums_positive = []
     spectrums_negative = []
     for i, spec in enumerate(spectrums):
@@ -86,7 +89,7 @@ def separate_spec(spectrums):
             print(f"No ionmode found for spectrum {i} ({spec.get('ionmode')})")
     return spectrums_positive,spectrums_negative
 
-def annotated(spectrums_pos_processing):
+def Annotated(spectrums_pos_processing):
     spectrums_pos_annotated = []
     for spec in tqdm(spectrums_pos_processing):
         inchikey = spec.get("inchikey")
@@ -118,7 +121,7 @@ def InstrumentFilter(spectrums_pos_annotated):
             other.append(spec)
     return orbitrap,qtof,other
 
-def make_dataset(spectrums_pos_annotated,n_max=100,test_size=0.2,n_decimals=2):
+def MakeDataset(spectrums_pos_annotated,n_max=100,test_size=0.2,n_decimals=2):
     smiles_unique = []
     train_ref = []
     train_query = []
@@ -151,13 +154,9 @@ def make_dataset(spectrums_pos_annotated,n_max=100,test_size=0.2,n_decimals=2):
             test_query.append([spec_list[p]])
             spec_list.pop(p)
             test_ref.append(spec_list)
-    # train_ref = pro_dataset(train_ref,n_decimals = n_decimals,n_max=n_max)
-    # train_query = pro_dataset(train_query,n_decimals = n_decimals,n_max=n_max)
-    # test_ref = pro_dataset(test_ref,n_decimals = n_decimals,n_max=n_max)
-    # test_query = pro_dataset(test_query,n_decimals = n_decimals,n_max=n_max)
     return train_ref,train_query,test_ref,test_query
 
-def pro_dataset(data,n_decimals,n_max):
+def ProDataset(data,n_decimals,n_max):
     data = [s for s1 in data for s in s1]
     data_info = []
     for spec in tqdm(data):
@@ -184,29 +183,29 @@ if __name__ == '__main__':
     gnps_file = 'E:/MSBERT_data/ALL_GNPS.msp'
     spectrums = list(load_from_msp(gnps_file))
     print("number of spectra:", len(spectrums))
-    count_annotations(spectrums) 
+    CountAnnotations(spectrums) 
     
-    spectrums = [apply_filters(s) for s in spectrums]
-    number_formula,fornuma_unique = count_formulas(spectrums)
-    spectrums = [clean_metadata(s) for s in tqdm(spectrums)]
-    spectrums = [clean_metadata2(s) for s in tqdm(spectrums)]
-    count_annotations(spectrums) 
+    spectrums = [ApplyFilters(s) for s in spectrums]
+    number_formula,fornuma_unique = CountFormulas(spectrums)
+    spectrums = [CleanMetadata(s) for s in tqdm(spectrums)]
+    spectrums = [CleanMetadata2(s) for s in tqdm(spectrums)]
+    CountAnnotations(spectrums) 
     
-    spectrums_positive,spectrums_negative = separate_spec(spectrums)
-    count_annotations(spectrums_positive)
+    spectrums_positive,spectrums_negative = SeparateSpec(spectrums)
+    CountAnnotations(spectrums_positive)
     
     number_of_peaks = np.array([len(s.peaks) for s in spectrums_positive])
     print(f"{np.sum(number_of_peaks < 5)} spectra have < 5 peaks")
     
     
     
-    spectrums_pos_processing = [minimal_processing(s) for s in spectrums_positive]
+    spectrums_pos_processing = [MinimalProcessing(s) for s in spectrums_positive]
     spectrums_pos_processing = [s for s in spectrums_pos_processing if s is not None]
-    count_annotations(spectrums_pos_processing)
+    CountAnnotations(spectrums_pos_processing)
     
-    spectrums_pos_annotated = annotated(spectrums_pos_processing)
+    spectrums_pos_annotated = Annotated(spectrums_pos_processing)
     pickle.dump(spectrums_pos_annotated, open('GNPSdata/spectrums_pos_annotated.pickle','wb'))
-    count_annotations(spectrums_pos_annotated)
+    CountAnnotations(spectrums_pos_annotated)
     
     with open('GNPSdata/spectrums_pos_annotated.pickle', 'rb') as f:
         spectrums_pos_annotated = pickle.load(f)
@@ -214,7 +213,7 @@ if __name__ == '__main__':
     orbitrap,qtof,other = InstrumentFilter(spectrums_filetr)
     
     #Save by instrument type
-    ob_train_ref,ob_train_query,ob_test_ref,ob_test_query =  make_dataset(orbitrap,n_max=99,test_size=0.2,n_decimals=2)
+    ob_train_ref,ob_train_query,ob_test_ref,ob_test_query =  MakeDataset(orbitrap,n_max=99,test_size=0.2,n_decimals=2)
     pickle.dump(ob_train_ref, open('GNPSdata/ob_train_ref.pickle','wb'))
     pickle.dump(ob_train_query, open('GNPSdata/ob_train_query.pickle','wb'))
     pickle.dump(ob_test_ref, open('GNPSdata/ob_test_ref.pickle','wb'))
