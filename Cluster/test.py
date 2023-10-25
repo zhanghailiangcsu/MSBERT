1b2f325bc147bac8345d9ee9ece3bc09a7da7d0d
zhanghailiangcsu <2352434994@qq.com>
2023-10-25 14:52:27 +0800

    Update test.py

--- Cluster/test.py
+++ Cluster/test.py
@@ -7,16 +7,17 @@ Created on Tue Oct 24 21:41:00 2023
 from model.utils import ModelEmbed,ParseOrbitrap
 from model.utils import ModelEmbed
 import torch
-from MSBERTModel import MSBERT
+from model.MSBERTModel import MSBERT
 from sklearn.cluster import Birch
 from sklearn import metrics
 from sklearn.metrics import rand_score,adjusted_rand_score,homogeneity_score
 from sklearn.metrics import homogeneity_completeness_v_measure,completeness_score
 import numpy as np
-
+from Spec2VecModel.TrainSpec2Vec import peak_embed
+from data.ProcessData import MakeTrainData
 
 def BirchCluster(n_clusters,test_arr):
-    brc = Birch(threshold=0.7,n_clusters=n_clusters)
+    brc = Birch(threshold=0.5,n_clusters=n_clusters)
     brc.fit(test_arr)
     return brc
 
@@ -52,15 +53,22 @@ if __name__ == '__main__':
     test_ref,msms3,precursor3,smiles3 = ParseOrbitrap('GNPSdata/ob_test_ref.pickle')
     MSBERTmodel = MSBERT(vocab_size, hidden, n_layers, attn_heads, dropout,maxlen,max_pred)
     MSBERTmodel.load_state_dict(torch.load('E:/MSBERT_model/1025/MSBERT.pkl'))
-    test_arr = ModelEmbed(MSBERTmodel,test_ref,batch_size)
+    
+    test_ref2,word2idx = MakeTrainData(msms3,precursor3,100)
+    test_arr = ModelEmbed(MSBERTmodel,test_ref2,batch_size)
+   
     smiles_unique = list(set(smiles3))
-    raw_label = GenRawLabel(smiles_unique,smiles3)
+    labels_true = GenRawLabel(smiles_unique,smiles3)
     
     n_clusters = len(smiles_unique)
     brc = BirchCluster(n_clusters,test_arr)
-    label = brc.predict(test_arr)
+    labels_pred = brc.predict(test_arr)
     # smiles = smiles3
-    result = CalEvaluate(label,raw_label)
+    result = CalEvaluate(labels_true, labels_pred)
+    model_file = 'Spec2vecModel/ob_all.model'
+    spectrums = [s for s1 in test_ref for s in s1]
+    spec2vec_vec = peak_embed(model_file,spectrums,n_decimals=2)
+    spec2vec_vec = np.array(spec2vec_vec)
     
     
 
